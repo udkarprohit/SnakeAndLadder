1e77fde7c8b03c5d787c7a5cdc254936b6fad49a
Prohit <udkarprohit@gmail.com>
2023-01-11 10:39:57 +0530

    generating random number b/w 1 to 6

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,9 +1,17 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
+    static int player1Pos = 0;
+
+    static int rollDie(){
+        int diceNum = ((int) (Math.random() * 10 ) % 6) + 1;
+        return diceNum;
+    }
     public static void main(String[] args) {
+
         System.out.println("Welcome to snake and ladder game");
-        int pos = 0;
-        System.out.println("Player position is : " + pos);
+        System.out.println("Player position is : " + player1Pos);
+        int dice = rollDie();
+        System.out.println("Random dice no : " + dice);
     }
 }
